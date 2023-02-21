2edb4ffabdf509482ac5b91ec6365ed735087ec4
luowei <forestandocean@126.com>
2023-02-21 17:16:04 +0800

    feat: test husky auto format

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -9,7 +9,7 @@ contract Greeter {
     string public greeting;
 
     constructor(string memory _greeting) {
-        console.log("Deploying Greeter with greeting:", _greeting);
+        console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
     }
 
